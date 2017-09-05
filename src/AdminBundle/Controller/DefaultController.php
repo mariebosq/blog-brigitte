e45b05ebb603cc3a2a1feb9a2387b868241484bf
Marie BOSQ <bosq.marie@gmail.com>
2017-09-05 15:33:36 +0200

    Ajout d'une action d'ajout d'un article

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -3,6 +3,13 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AdminBundle\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DefaultController extends Controller
 {
@@ -10,4 +17,51 @@ class DefaultController extends Controller
     {
         return $this->render('AdminBundle:Default:index.html.twig');
     }
+
+    public function addAction(Request $request)
+  {
+    // On crée un objet Article
+    $article = new Article();
+
+    // On crée le FormBuilder grâce au service form factory
+    $form = $this->get('form.factory')->createBuilder(FormType::class, $article)
+      ->add('publishedAt',      DateType::class)
+      ->add('createdAt',      DateType::class)
+      ->add('updatedAt',      DateType::class)
+      ->add('title',     TextType::class)
+      ->add('slug',     TextType::class)
+      ->add('content',   TextareaType::class)
+      //->add('published', CheckboxType::class, array('required' => false))
+      ->add('save',      SubmitType::class)
+      ->getForm()
+    ;
+
+    // Si la requête est en POST
+    if ($request->isMethod('POST')) {
+      // On fait le lien Requête <-> Formulaire
+      // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+      $form->handleRequest($request);
+
+      // On vérifie que les valeurs entrées sont correctes
+      // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+      if ($form->isValid()) {
+        // On enregistre notre objet $advert dans la base de données, par exemple
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+        // On redirige vers la page de visualisation de l'annonce nouvellement créée
+        return $this->redirectToRoute('admin_homepage', array('id' => $advert->getId()));
+      }
+    }
+
+    // À ce stade, le formulaire n'est pas valide car :
+    // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+    // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+    return $this->render('AdminBundle:Default:form.html.twig', array(
+      'form' => $form->createView(),
+    ));
+  }
 }
