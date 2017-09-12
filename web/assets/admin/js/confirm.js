90703fa7af18ad60ff0238ec08c4388d04596c56
Marie BOSQ <bosq.marie@gmail.com>
2017-09-12 16:11:45 +0200

    Ajout de la fenêtre de confirmation pour la publication d'un article

--- web/assets/admin/js/confirm.js
+++ web/assets/admin/js/confirm.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//Gestion de la fenêtre pour le lien de suppression
 $(document).ready(function() {
     var theHREF;
 
@@ -24,5 +25,34 @@ $(document).ready(function() {
         e.preventDefault();
         theHREF = $(this).attr("href");
         $("#dialog-confirm").dialog("open");
+    });
+});
+
+//Gestion de la fenêtre pour le lien de la publication
+$(document).ready(function() {
+    var theHREF;
+
+    $("#dialog-confirm-modif").dialog({
+        resizable: false,
+        height:160,
+        width:500,
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "Oui": function() {
+                $(this).dialog("close");
+                window.location.href = theHREF;
+            },
+            "Annuler": function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    $("a.confirmModalp").click(function(e) {
+
+        e.preventDefault();
+        theHREF = $(this).attr("href");
+        $("#dialog-confirm-modif").dialog("open");
     });
 });
