602de8db0409e6d77655b3f7490fa5d37b91c699
Marie BOSQ <bosq.marie@gmail.com>
2017-09-12 14:29:10 +0200

    Ajout de dossier jquery et du code pour animer la fenêtre de ocnfirmation de suppression

---
+++ web/assets/admin/js/confirm.js
@@ -0,0 +1,28 @@
+'use strict'
+
+$(document).ready(function() {
+    var theHREF;
+
+    $("#dialog-confirm").dialog({
+        resizable: false,
+        height:160,
+        width:500,
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "Oui": function() {
+                $(this).dialog("close");
+                window.location.href = theHREF;
+            },
+            "Annuler": function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    $("a.confirmModal").click(function(e) {
+        e.preventDefault();
+        theHREF = $(this).attr("href");
+        $("#dialog-confirm").dialog("open");
+    });
+});
